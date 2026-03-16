26d59e90ddf6729e7618daed7f6c6e8e4ab50f3b
piyush <piyushnagbansi500@gmail.com>
2026-03-16 19:31:31 +0530

    Fix Nhost connectivity with correct subdomain

--- src/lib/nhost.js
+++ src/lib/nhost.js
@@ -1,8 +1,6 @@
 import { NhostClient } from '@nhost/react'
 
 export const nhost = new NhostClient({
-  // Replace 'ap-south-1' with your actual 12-character Nhost subdomain
-  // You can find it in your Nhost dashboard (Settings -> General)
-  subdomain: 'YOUR_NHOST_SUBDOMAIN',
+  subdomain: 'jtqbvxjrvhvg',
   region: 'ap-south-1'
 })
